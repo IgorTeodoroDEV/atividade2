4d23b58c98346968727fb11adeb1b302cfa3bb9d
IgorTeodoroDEV <igorteodoro2409@gmail.com>
2024-06-13 10:03:22 -0300

    atualizando

--- assests/classes/carrinho.php
+++ assests/classes/carrinho.php
@@ -1,21 +1,4 @@
 <div class="client">
-    <?php
-        session_start();
-
-        if (!isset($_SESSION['carrinho'])) {
-            $_SESSION['carrinho'] = array();
-        }
-
-        echo "<h1>Carrinho de Compras</h1>";
-
-        if (empty($_SESSION['carrinho'])) {
-            echo "<p>O carrinho está vazio.</p>";
-        } else {
-            echo "<ul>";
-            foreach ($_SESSION['carrinho'] as $produto) {
-                echo "<li>$produto</li>";
-            }
-            echo "</ul>";
-        }
-    ?>
+    <h1>Carrinho de Compras</h1>
+    <p>O carrinho está vazio.</p>
 </div>
